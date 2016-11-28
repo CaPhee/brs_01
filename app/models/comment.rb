98096e328dabefb2c350a726dcc8f6cb6619d812
quangtien133 <phanqtien@gmail.com>
2016-11-28 14:43:42 +0700

    comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :commentator
 
+  validates :commentator, presence: true
   validates :content, presence: true,
     length: {maximum: Settings.max_lenght_content_comment}
 
@@ -10,4 +11,13 @@ class Comment < ApplicationRecord
   delegate :user, to: :commentator, allow_nil: true
   delegate :title, to: :commentator, allow_nil: true
   delegate :review, to: :commentator, allow_nil: true
+  delegate :review_id, to: :commentator, allow_nil: true
+
+  after_create :create_activity
+
+  private
+  def create_activity
+    Activity.create user_id: user.id, target_id: id, target_type: Comment.name,
+      action_type: Activity.activity_types[:created]
+  end
 end
