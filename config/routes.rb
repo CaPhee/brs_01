93c76940b0e1c1a750c27d6de94d2d45d8d41708
quangtien133 <phanqtien@gmail.com>
2016-11-29 11:21:52 +0700

    user like/unlike an activities

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
   resources :requests, except: [:index]
+  resources :likes, only: [:create, :destroy]
 end
